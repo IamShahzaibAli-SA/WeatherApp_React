8396ed792ecd06da380244066518b0e1f57fedfa
IamShahzaibAli-SA <243137271+IamShahzaibAli-SA@users.noreply.github.com>
2025-11-26 19:34:41 +0500

    Custom Hook Is Almost Ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import { useState } from 'react'
 import './App.css'
+import useWeatherInfo from './hooks/useWeatherInfo'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [city, setCity] = useState("karachi");
 
+  const data = useWeatherInfo(city);
   return (
     <>
-      
+    <div className='flex justify-center'>
+      <input 
+      type="text"
+      className='bg-white'
+
+      />
+    </div>
     </>
   )
 }
---
+++ src/hooks/useWeatherInfo.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+
+function useWeatherInfo(city){
+    const [update, setUpdate] = useState(null);
+
+    useEffect(()=>{
+        const apiKey = "1035163e744b42001d78406927c365ba";
+        const fetchAPI = async () => {
+            const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+            const response = await fetch(apiURL);
+            const result = await response.json();
+            setUpdate(result[name]);
+    
+            console.log(result);
+            console.log(result.main.temp);
+            console.log(result.name);
+        }
+    
+        fetchAPI();
+    }, []);
+
+    return update;
+}
+
+export default useWeatherInfo;
